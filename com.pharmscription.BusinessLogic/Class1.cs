8f13228266991a7da73feb66bae64e1c3c761519
Markus Schaden <mrksschdn@gmail.com>
2016-03-29 17:16:23 +0200

    Created project structure

---
+++ com.pharmscription.BusinessLogic/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.pharmscription.BusinessLogic
+{
+    public class Class1
+    {
+    }
+}
---
+++ com.pharmscription.DataAccess.Insurance.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace com.pharmscription.DataAccess.Insurance.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
