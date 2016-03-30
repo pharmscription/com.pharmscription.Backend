b1158b9c4d8985c22429566442a293474873d7ce
Oliviero Chiodo <oliviero.chiodo@gmail.com>
2016-03-30 23:23:45 +0200

    ADD: PatientService

---
+++ com.pharmscription.Service/Patient/IPatientService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using com.pharmscription.Infrastructure.Dto;
+
+namespace com.pharmscription.Service.Patient
+{
+    // HINWEIS: Mit dem Befehl "Umbenennen" im Menü "Umgestalten" können Sie den Schnittstellennamen "IPatientService" sowohl im Code als auch in der Konfigurationsdatei ändern.
+    [ServiceContract]
+    /*[AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]*/
+    public interface IPatientService
+    {
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            RequestFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "patients/{id}")]
+        [OperationContract]
+        PatientDto GetPatient(Guid id);
+
+        [WebInvoke(Method = "PUT",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "patients")]
+        [OperationContract]
+        PatientDto CreatePatient(PatientDto dto);
+
+        [WebInvoke(Method = "POST",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "patients/{id}")]
+        [OperationContract]
+        PatientDto ModifyPatient(PatientDto dto);
+
+        [WebInvoke(Method = "GET",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "patients/{id}/address")]
+        [OperationContract]
+        AddressDto GetAddress(Guid patientId);
+
+        [WebInvoke(Method = "GET",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "patients/ahv-number/{ahv}")]
+        [OperationContract]
+        PatientDto GetPatientByAhv(string ahv);
+
+        [WebInvoke(Method = "DELETE",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "patients/{id}")]
+        [OperationContract]
+        PatientDto DeletePatient(Guid id);
+    }
+}
+}
---
+++ com.pharmscription.Service/Patient/PatientService.svc.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using com.pharmscription.Infrastructure.Dto;
+
+namespace com.pharmscription.Service.Patient
+{
+    // HINWEIS: Mit dem Befehl "Umbenennen" im Menü "Umgestalten" können Sie den Klassennamen "PatientService" sowohl im Code als auch in der SVC- und der Konfigurationsdatei ändern.
+    // HINWEIS: Wählen Sie zum Starten des WCF-Testclients zum Testen dieses Diensts PatientService.svc oder PatientService.svc.cs im Projektmappen-Explorer aus, und starten Sie das Debuggen.
+    public class PatientService : IPatientService
+    {
+        public PatientDto CreatePatient(PatientDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PatientDto DeletePatient(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AddressDto GetAddress(Guid patientId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PatientDto GetPatient(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PatientDto GetPatientByAhv(string ahv)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PatientDto ModifyPatient(PatientDto dto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
