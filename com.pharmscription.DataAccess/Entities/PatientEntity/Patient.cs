deb6146b311c0859f7ca3d57dfc9c8356381e775
ValisStigma <rafael_krucker@hotmail.com>
2016-03-30 09:17:29 +0200

    Added Repository Interface for Patients

--- com.pharmscription.DataAccess/Entities/PatientEntity/Patient.cs
+++ com.pharmscription.DataAccess/Entities/PatientEntity/Patient.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using com.pharmscription.DataAccess.Entities.AddressEntity;
 using com.pharmscription.DataAccess.Entities.BaseEntity;
 
--- com.pharmscription.DataAccess/Repositories/Patient/IPatientRepository.cs
+++ com.pharmscription.DataAccess/Repositories/Patient/IPatientRepository.cs
@@ -1,20 +1,11 @@
-﻿using System;
-
-using com.pharmscription.DataAccess.SharedInterfaces;
+﻿using com.pharmscription.DataAccess.SharedInterfaces;
 
 namespace com.pharmscription.DataAccess.Repositories.Patient
 {
-  /*  public interface IPatientRepository : IRepository<Patient>
+    public interface IPatientRepository : IRepository<Entities.PatientEntity.Patient>
     {
+        Entities.PatientEntity.Patient GetByAhvNumber(string ahvNumber);
+        bool Exists(string ahvNumber);
+       
     }
-
-    public interface IAccountRepository : IRepository<Account>
-    {
-        Account Get(string username);
-        bool Exists(string username);
-        Account GetByEmailActivationCode(string activationCode);
-        Account GetByIdentityId(Guid identityId);
-        Account GetByPasswordResetCode(string passwordResetCode);
-        Account GetByEmailChangeRequestCode(string emailChangeRequestCode);
-    }*/
 }
