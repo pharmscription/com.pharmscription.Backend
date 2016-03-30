5c766dfdb2c2aab82e2cb41eb7de556abb6362c9
ValisStigma <rafael_krucker@hotmail.com>
2016-03-30 09:03:50 +0200

    Started Interface Implementation

---
+++ com.pharmscription.DataAccess/Repositories/Patient/IPatientRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using com.pharmscription.DataAccess.SharedInterfaces;
+
+namespace com.pharmscription.DataAccess.Repositories.Patient
+{
+  /*  public interface IPatientRepository : IRepository<Patient>
+    {
+    }
+
+    public interface IAccountRepository : IRepository<Account>
+    {
+        Account Get(string username);
+        bool Exists(string username);
+        Account GetByEmailActivationCode(string activationCode);
+        Account GetByIdentityId(Guid identityId);
+        Account GetByPasswordResetCode(string passwordResetCode);
+        Account GetByEmailChangeRequestCode(string emailChangeRequestCode);
+    }*/
+}
