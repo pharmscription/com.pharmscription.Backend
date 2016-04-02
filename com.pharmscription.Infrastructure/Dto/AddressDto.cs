732b9f6e6f2efd61eca524a77b663666fc8fc7e9
Oliviero Chiodo <oliviero.chiodo@gmail.com>
2016-04-02 13:41:12 +0200

    MOD: add street extension to address entity & dto

--- com.pharmscription.Infrastructure/Dto/AddressDto.cs
+++ com.pharmscription.Infrastructure/Dto/AddressDto.cs
@@ -4,6 +4,7 @@ namespace com.pharmscription.Infrastructure.Dto
     public class AddressDto
     {
         public string Street { get; set; }
+        public string StreetExtension { get; set; }
         public string Number { get; set; }
         public string City { get; set; }
         public string CityCode { get; set; }
