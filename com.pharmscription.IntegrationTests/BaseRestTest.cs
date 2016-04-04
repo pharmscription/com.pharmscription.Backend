c1af3c0f20354fbf0033b41960b3a2185574635f
Markus Schaden <mrksschdn@gmail.com>
2016-04-04 17:45:34 +0200

    CORS Fix

---
+++ com.pharmscription.IntegrationTests/BaseRestTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using com.pharmscription.Service;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace com.pharmscription.IntegrationTests
+{
+    [TestClass]
+    public class BaseRestTest
+    {
+        public RestService Client;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            Client = new RestService();
+        }   
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+           
+        }
+    }
+}
