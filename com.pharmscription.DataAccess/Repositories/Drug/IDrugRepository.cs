dcb7a77bad6b18aebab67d6f5d84b356a0dfc7db
ValisStigma <rafael_krucker@hotmail.com>
2016-04-04 22:11:29 +0200

    Implemented Drug Entity, Added Interface for Drugrepository

---
+++ com.pharmscription.DataAccess/Repositories/Drug/IDrugRepository.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using com.pharmscription.DataAccess.SharedInterfaces;
+
+namespace com.pharmscription.DataAccess.Repositories.Drug
+{
+    public interface IDrugRepository : IRepository<Entities.DrugEntity.Drug>
+    {
+        Task<IEnumerable<DataAccess.Entities.DrugEntity.Drug>> SearchByName(string name);
+        Task<DataAccess.Entities.DrugEntity.Drug> GetById(Guid id);
+
+    }
+}
