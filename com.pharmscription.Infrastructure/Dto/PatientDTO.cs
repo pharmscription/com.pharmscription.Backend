a4bf2c90cec658cfccf276ddfc754a38e425e1ea
Oliviero Chiodo <oliviero.chiodo@gmail.com>
2016-04-05 13:45:06 +0200

    MOD: Datacontract to dto

--- com.pharmscription.Infrastructure/Dto/PatientDTO.cs
+++ com.pharmscription.Infrastructure/Dto/PatientDTO.cs
@@ -1,17 +1,28 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace com.pharmscription.Infrastructure.Dto
 {
+    [DataContract]
     public class PatientDto : BaseDto
     {
+        [DataMember]
         public string FirstName { get; set; }
+        [DataMember]
         public string LastName { get; set; }
+        [DataMember]
         public string EMailAddress { get; set; }
+        [DataMember]
         public string AhvNumber { get; set; }
+        [DataMember]
         public AddressDto Address { get; set; }
+        [DataMember]
         public DateTime BirthDate { get; set; }
+        [DataMember]
         public string PhoneNumber { get; set; }
+        [DataMember]
         public string InsuranceNumber { get; set; }
+        [DataMember]
         public string Insurance { get; set; }
     }
 }
