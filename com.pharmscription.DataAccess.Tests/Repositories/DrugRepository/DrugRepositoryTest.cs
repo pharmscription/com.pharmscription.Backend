f281a2aba1f9aa9d30a2d70d81469d0448029c88
ValisStigma <rafael_krucker@hotmail.com>
2016-04-05 08:33:41 +0200

    Started tests for drugrepository

---
+++ com.pharmscription.DataAccess.Tests/Repositories/DrugRepository/DrugRepositoryTest.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using com.pharmscription.DataAccess.Repositories.Drug;
+using com.pharmscription.DataAccess.Repositories.Patient;
+using com.pharmscription.DataAccess.Tests.TestEnvironment;
+using com.pharmscription.DataAccess.UnitOfWork;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace com.pharmscription.DataAccess.Tests.Repositories.DrugRepository
+{
+    [TestClass]
+    public class DrugRepositoryTest
+    {
+        private IPharmscriptionUnitOfWork _puow;
+        private IDrugRepository _repository;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            var patients = new List<DataAccess.Entities.PatientEntity.Patient>
+            {
+                new DataAccess.Entities.PatientEntity.Patient
+                {
+                    AhvNumber = "123",
+                    FirstName = "Rafael",
+                    BirthDate = new DateTime(1991, 03, 17)
+                },
+                new DataAccess.Entities.PatientEntity.Patient
+                {
+                    AhvNumber = "124",
+                    FirstName = "Noah",
+                    BirthDate = new DateTime(1990, 03, 17)
+                },
+                new DataAccess.Entities.PatientEntity.Patient
+                {
+                    AhvNumber = "125",
+                    FirstName = "Markus",
+                    BirthDate = new DateTime(1998, 03, 17)
+                },
+                new DataAccess.Entities.PatientEntity.Patient
+                {
+                    AhvNumber = "126",
+                    FirstName = "Pascal",
+                    BirthDate = new DateTime(1987, 03, 17)
+                },
+                new DataAccess.Entities.PatientEntity.Patient
+                {
+                    AhvNumber = "127",
+                    FirstName = "Oliviero",
+                    BirthDate = new DateTime(1983, 03, 17)
+                }
+            };
+            var mockSet = TestEnvironmentHelper.GetMockedAsyncProviderDbSet(patients);
+
+            var mockPuow = TestEnvironmentHelper.GetMockedDataContext();
+            mockPuow.Setup(m => m.Patients).Returns(mockSet.Object);
+            mockPuow.Setup(m => m.CreateSet<DataAccess.Entities.PatientEntity.Patient>()).Returns(mockSet.Object);
+            _puow = mockPuow.Object;
+/*
+            _repository = new PatientRepository(_puow);
+*/
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+
+            IPharmscriptionUnitOfWork puow = new PharmscriptionUnitOfWork();
+
+            foreach (var id in puow.Patients.Select(e => e.Id))
+            {
+                var entity = new DataAccess.Entities.PatientEntity.Patient { Id = id };
+                puow.Patients.Attach(entity);
+                puow.Patients.Remove(entity);
+            }
+            puow.Commit();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
