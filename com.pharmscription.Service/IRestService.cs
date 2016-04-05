e3fba84c0402c10d32a50420da1faabc05980eda
Oliviero Chiodo <oliviero.chiodo@gmail.com>
2016-04-05 16:26:25 +0200

    MOD: add drug interface to service

--- com.pharmscription.Service/IRestService.cs
+++ com.pharmscription.Service/IRestService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.ServiceModel;
 using System.ServiceModel.Web;
+
 using com.pharmscription.Infrastructure.Dto;
 
 namespace com.pharmscription.Service
@@ -59,7 +60,35 @@ namespace com.pharmscription.Service
             UriTemplate = "patients/{id}")]
         [OperationContract]
         PatientDto DeletePatient(string id);
+
+        #endregion
+
+        #region drugs
+
+        [WebInvoke(Method = "GET", 
+            RequestFormat = WebMessageFormat.Json, 
+            ResponseFormat = WebMessageFormat.Json, 
+            BodyStyle = WebMessageBodyStyle.Bare, 
+            UriTemplate = "drugs/{id}")]
+        [OperationContract]
+        DrugDto GetDrug(string id);
+
+        [WebInvoke(Method = "GET", 
+            RequestFormat = WebMessageFormat.Json, 
+            ResponseFormat = WebMessageFormat.Json, 
+            BodyStyle = WebMessageBodyStyle.Bare, 
+            UriTemplate = "drugs/search/{keyword}")]
+        [OperationContract]
+        DrugDto[] SearchDrugs(string keyword);
+
+        [WebInvoke(Method = "GET", 
+            RequestFormat = WebMessageFormat.Json, 
+            ResponseFormat = WebMessageFormat.Json, 
+            BodyStyle = WebMessageBodyStyle.Wrapped, 
+            UriTemplate = "drugs/{id}/price")]
+        [OperationContract]
+        double GetDrugPrice(string id);
+
         #endregion
     }
 }
-
