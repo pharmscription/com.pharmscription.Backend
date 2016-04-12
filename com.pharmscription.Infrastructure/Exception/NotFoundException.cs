d640c032b01974fdb1412abed690e1262f7d7e2f
Oliviero Chiodo <oliviero.chiodo@gmail.com>
2016-04-12 23:45:04 +0200

    ADD: notfount and invalidargument exceptions

---
+++ com.pharmscription.Infrastructure/Exception/NotFoundException.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.pharmscription.Infrastructure.Exception
+{
+    using System.Runtime.Serialization;
+
+    using Exception = System.Exception;
+
+    public class NotFoundException: System.Exception
+    {
+        public NotFoundException()
+        {
+            
+        }
+
+        public NotFoundException(string message)
+            : base(message)
+        {
+            
+        }
+
+        public NotFoundException(string message, Exception innerException):
+            base(message, innerException)
+        {
+            
+        }
+
+        public NotFoundException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+            
+        }
+    }
+}
