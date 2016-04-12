7283625a218a480bf5ad3c082f982a4e13e537a9
Oliviero Chiodo <oliviero.chiodo@gmail.com>
2016-04-12 09:51:24 +0200

    ADD: error message class

---
+++ com.pharmscription.Service/ErrorMessage.cs
@@ -0,0 +1,19 @@
+﻿namespace com.pharmscription.Service
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Net;
+    using System.Runtime.Serialization;
+
+    [DataContract]
+    public class ErrorMessage
+    { 
+        [DataMember(Name = "message")]
+        public string Message { get; set; }
+
+        public ErrorMessage(string message)
+        {
+            Message = message;
+        }
+    }
+}
