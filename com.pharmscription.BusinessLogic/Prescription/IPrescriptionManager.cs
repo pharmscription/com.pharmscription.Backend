eacd8eff6218f3df769d38359b3c21af0fcb9a3c
Rafael Krucker <rafael_krucker@hotmail.com>
2016-04-18 08:31:09 +0200

    Startet work on business layer

---
+++ com.pharmscription.BusinessLogic/Prescription/IPrescriptionManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.pharmscription.BusinessLogic.Prescription
+{
+/*    public interface IPrescriptionManager
+    {
+        Task<List<PrescriptionDro>>
+    }*/
+}
---
+++ com.pharmscription.Infrastructure/Dto/PrescriptionDto.cs
@@ -0,0 +1,10 @@
+﻿
+using System.Runtime.Serialization;
+
+namespace com.pharmscription.Infrastructure.Dto
+{
+    [DataContract]
+    public class PrescriptionDto : BaseDto
+    {
+    }
+}
