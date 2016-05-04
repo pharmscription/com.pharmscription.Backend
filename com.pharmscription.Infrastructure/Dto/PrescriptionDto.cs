39927b58bb85193bd088d4dc68ad811e853edcf4
Rafael Krucker <rafael_krucker@hotmail.com>
2016-05-04 13:07:30 +0200

    Added suppress warning messages

--- com.pharmscription.Infrastructure/Dto/PrescriptionDto.cs
+++ com.pharmscription.Infrastructure/Dto/PrescriptionDto.cs
@@ -4,7 +4,6 @@ namespace com.pharmscription.Infrastructure.Dto
     ﻿using System.Diagnostics.CodeAnalysis;
     ﻿using System.Runtime.Serialization;
     [DataContract]
-    [SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
     public class PrescriptionDto : BaseDto
     {
         [DataMember]
@@ -22,12 +21,16 @@ namespace com.pharmscription.Infrastructure.Dto
         [DataMember]
         public bool IsValid { get; set; }
         [DataMember]
+        [SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public IReadOnlyCollection<CounterProposalDto> CounterProposals { get; set; }
         [DataMember]
+        [SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public IReadOnlyCollection<DispenseDto> Dispenses { get; set; }
         [DataMember]
+        [SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public IReadOnlyCollection<DrugItemDto> Drugs { get; set; }
         [DataMember]
+        [SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public IReadOnlyCollection<PrescriptionDto> PrescriptionHistory { get; set; }
     }
 }
