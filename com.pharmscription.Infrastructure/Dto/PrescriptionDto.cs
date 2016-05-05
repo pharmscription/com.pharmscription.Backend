7edec63c0d5605f33e993601dcdfd9f6d09a77f3
Oliviero Chiodo <oliviero.chiodo@gmail.com>
2016-05-05 14:51:49 +0200

    MOD: remove datacontract

--- com.pharmscription.Infrastructure/Dto/PrescriptionDto.cs
+++ com.pharmscription.Infrastructure/Dto/PrescriptionDto.cs
@@ -1,31 +1,20 @@
+using System.Collections.Generic;
+using System.Runtime.Serialization;
 namespace com.pharmscription.Infrastructure.Dto
 {
-    ﻿using System.Collections.Generic;
-    ﻿using System.Runtime.Serialization;
-    [DataContract]
     public class PrescriptionDto : BaseDto
     {
-        [DataMember]
         public string Type { get; set; }
-        [DataMember]
         public PatientDto Patient { get; set; }
-        [DataMember]
         public string IssueDate { get; set; }
-        [DataMember]
         public string EditDate { get; set; }
-        [DataMember]
-        public string SignDate { get; set; }
-        [DataMember]
+        //TODO: remove
+        //public string SignDate { get; set; }
         public string ValidUntil { get; set; }
-        [DataMember]
         public bool IsValid { get; set; }
-        [DataMember]
         public IReadOnlyCollection<CounterProposalDto> CounterProposals { get; set; }
-        [DataMember]
         public IReadOnlyCollection<DispenseDto> Dispenses { get; set; }
-        [DataMember]
         public IReadOnlyCollection<DrugItemDto> Drugs { get; set; }
-        [DataMember]
         public IReadOnlyCollection<PrescriptionDto> PrescriptionHistory { get; set; }
     }
 }
